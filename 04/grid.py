deb7ffe5514d20ea8bd8ebcce54f400bbd6dc1e8
groundkcm <karaminseo@naver.com>
2021-09-09 21:00:21 +0900

    DRILL 04 완성
    2020180017 박민서

--- 04/grid.py
+++ 04/grid.py
@@ -1,5 +1,9 @@
 import turtle
 
+turtle.penup()
+turtle.goto(0,-300)
+turtle.pendown()
+
 num = 4
 while(num > 0):
     turtle.forward(500)
@@ -7,7 +11,7 @@ while(num > 0):
     num -= 1
 
 x = 0
-y = 0
+y = -300
 while(num < 4):
     y += 100
     turtle.penup()
